0a3622e256a94735d98acd2e910bfef556763c6e
Jovial_Msc <mbonnoujovial@gmail.com>
2023-11-27 16:32:42 +0100

    data to Data in utils/read_data.py

--- utils/read_data.py
+++ utils/read_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def read_data():
-    weather_df = pd.read_csv('../data/wetter_Kiel_2015_2019.csv', sep=';', decimal=',', parse_dates=['Date'], dayfirst=True)
+    weather_df = pd.read_csv('../Data/wetter_Kiel_2015_2019.csv', sep=';', decimal=',', parse_dates=['Date'], dayfirst=True)
 
     # Weather Cloud Cover: https://en.wikipedia.org/wiki/Okta
     # Weather Code:  https://wetterkanal.kachelmannwetter.com/was-ist-der-ww-code-in-der-meteorologie/
@@ -10,14 +10,14 @@ def read_data():
     weather_df.sort_values(by=['Date'], inplace=True)
     
     
-    sales_df = pd.read_csv('../data/hackathon_meteolytix_Artikelgruppen_Umsatz_verschiedeneStandorte.csv', parse_dates=['Datum'])
+    sales_df = pd.read_csv('../Data/hackathon_meteolytix_Artikelgruppen_Umsatz_verschiedeneStandorte.csv', parse_dates=['Datum'])
     sales_df = sales_df.rename(columns={'Datum': 'Date', 'Umsatz': 'Sales', 'Artikelgruppe': 'Product Group'})
     sales_df = sales_df.astype({'Filiale': 'category', 'Product Group': 'category'})
     # sort by date
     sales_df.sort_values(by=['Date'], inplace=True)
     
     
-    product_group_df = pd.read_csv('../data/meteolytix_Artikelgruppen.csv', sep=';')
+    product_group_df = pd.read_csv('../Data/meteolytix_Artikelgruppen.csv', sep=';')
     # change to dictionary
     product_group_dict = product_group_df.set_index('Artikelgruppe').to_dict()['Name']
     
